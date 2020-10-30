78fcabb54230140ec0e3363ac6f3630a9a7d5ce3
Theeraphat63070087 <73682336+Theeraphat63070087@users.noreply.github.com>
2020-10-30 15:25:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,19 @@
     </head>
     <body>
         <h1>Name : Theeraphat Samboonruang</h1>
-        <p>Nickname : Matthew</p>
+        <p>Nickname : Aum</p>
         <img src="Alice.png">
         <img src="Emilia.png">
         <br>
-        <a href="https://www.apple.com/">Favorite Website</a>
+        <a href="https://www.apple.com/">My Favorite Website</a>
 
-        <h2>The unordered list</h2>
+        <h2>SHORT STORY</h2>
         <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <li>สมชายเดินชมพิพิธภัณฑ์และหยุดที่หน้าโครงกระดูกไดโนเสาร์</li>
+            <li>หันไปบอกกับคนที่ยืนข้างๆ</li>
+            <li>"คุณรู้มั๊ย ไดโนเสาร์ตัวนี้อายุ 100 ล้านปีกับอีก 10เดือน"</li>
+            <li>ชายคนนั้นมองด้วยความทึ่งแล้วถามว่า"ทำไมคุณรู้ละเอียดขนาดนั้นเลยล่ะ"</li>
+            <li>"ก็ผมมาที่นี่เมื่อ 10 เดือนที่แล้วไกด์บอกผมว่าเจ้าตัวนี้มันอายุ 100ล้านปีนะสิ</li>
         </ul>
     
     </body>
